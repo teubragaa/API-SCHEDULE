eb0e14f6f39e4dd28c5cf3b183c311a1516f1ae8
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-08-18 16:49:32 -0300

    Adiciona repository

--- src/main/java/com/example/schedule/repository/DisciplinesRepository.java
+++ src/main/java/com/example/schedule/repository/DisciplinesRepository.java
@@ -3,5 +3,5 @@ package com.example.schedule.repository;
 import com.example.schedule.entity.Discipline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
+public interface DisciplinesRepository extends JpaRepository<Discipline, Long> {
 }
--- src/main/java/com/example/schedule/repository/TeacherRepository.java
+++ src/main/java/com/example/schedule/repository/TeacherRepository.java
@@ -1,7 +1,6 @@
 package com.example.schedule.repository;
-
 import com.example.schedule.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository <Teacher, Long>{
 }
