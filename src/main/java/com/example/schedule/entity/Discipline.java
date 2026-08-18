23f78f8c44dcec5497b7e7d41c3a186741c4f6a1
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-08-18 11:46:28 -0300

    Entity Discipline.java

--- src/main/java/com/example/schedule/entity/Discipline.java
+++ src/main/java/com/example/schedule/entity/Discipline.java
@@ -1,4 +1,34 @@
 package com.example.schedule.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "disciplines")
 public class Discipline {
+
+    public Discipline() {
+    }
+
+    public Discipline(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
