90e5af5474ab5177a8589541fff8740640c8eccf
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-08-18 17:34:54 -0300

    Adiciona @Postmapping create e getmapping findall

--- src/main/java/com/example/schedule/controller/TeacherController.java
+++ src/main/java/com/example/schedule/controller/TeacherController.java
@@ -1,4 +1,28 @@
 package com.example.schedule.controller;
 
+import com.example.schedule.entity.Teacher;
+import com.example.schedule.service.TeacherService;
+import org.apache.coyote.Request;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/teacher")
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping
+    public Teacher create(RequestBody Teacher teacher){
+        return teacherService.create(teacher);
+    }
+    @GetMapping
+    public List<Teacher> findAll(){
+        return teacherService.findAll();
+    }
 }
