9351de8bdddfeb9ab4383c753c14c6c3e0dbe680
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-08-18 11:25:10 -0300

    Controller Teacher.java

--- src/main/java/com/example/schedule/entity/Teacher.java
+++ src/main/java/com/example/schedule/entity/Teacher.java
@@ -1,4 +1,47 @@
 package com.example.schedule.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "teachers")
 public class Teacher {
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
