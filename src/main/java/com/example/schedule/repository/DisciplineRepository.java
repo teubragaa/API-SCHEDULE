d52d1eba9807387b53b7a5a8d6da0a498df121d8
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-08-18 11:45:11 -0300

    Repository DisciplineRepository.java

--- src/main/java/com/example/schedule/repository/DisciplineRepository.java
+++ src/main/java/com/example/schedule/repository/DisciplineRepository.java
@@ -1,4 +1,7 @@
 package com.example.schedule.repository;
 
-public class DisciplineRepository {
+import com.example.schedule.entity.Discipline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
 }
